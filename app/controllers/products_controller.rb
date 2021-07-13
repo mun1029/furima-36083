d909a8e91550f299fbd59886ca9ae6b2f45d5538
mun1029 <ring01061995@gmail.com>
2021-07-13 20:46:58 +0900

    商品情報編集機能の実装

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,7 @@
 class ProductsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :set_product, only: [:show, :edit, :update]
+  before_action :move_to_edit,  only: [:edit]
 
   def index
     @products = Product.all.order("created_at DESC")
@@ -19,11 +21,31 @@ class ProductsController < ApplicationController
   end
 
   def show
-    @product = Product.find(params[:id])
+  end
+
+  def edit
+  end
+
+  def update
+    if @product.update(product_params)
+      redirect_to products_path
+    else
+      render :edit
+    end
   end
 
   private
   def product_params
     params.require(:product).permit(:name, :description, :category_id, :status_id, :delivery_id, :area_id, :delivery_day_id, :price, :image).merge(user_id: current_user.id)
+  end
+
+  def set_product
+    @product = Product.find(params[:id])
+  end
+
+  def move_to_edit
+    unless current_user.id == @product.user.id
+      redirect_to action: :index
+    end
   end
 end
